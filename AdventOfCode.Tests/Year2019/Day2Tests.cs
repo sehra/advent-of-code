441d583252c3236275fe6c9c28a23fe4858d970b
Martin Wallgren <martin@orangeworks.se>
2019-12-03 15:19:32 +0100

    Day 2

---
+++ AdventOfCode.Tests/Year2019/Day2Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2019
+{
+	[TestClass]
+	public class Day2Tests
+	{
+		[DataTestMethod]
+		[DataRow("1,9,10,3,2,3,11,0,99,30,40,50", 0, 3500)]
+		[DataRow("1,0,0,0,99", 0, 2)]
+		[DataRow("2,3,0,3,99", 3, 6)]
+		[DataRow("2,4,4,5,99,0", 5, 9801)]
+		[DataRow("1,1,1,4,99,5,6,0,99", 0, 30)]
+		public void Computer(string input, int address, int expected)
+		{
+			var memory = input.Split(',').Select(Int32.Parse).ToArray();
+			var intcode = new Day2.Computer(memory);
+			intcode.Run();
+
+			Assert.AreEqual(expected, intcode.Get(address));
+		}
+	}
+}
---
+++ AdventOfCode/Year2019/Day2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Year2019
+{
+	public class Day2
+	{
+		private readonly int[] _input;
+
+		public Day2(string input)
+		{
+			_input = input.Split(',').Select(Int32.Parse).ToArray();
+		}
+
+		public int Part1()
+		{
+			var intcode = new Computer(_input);
+			intcode.Set(1, 12);
+			intcode.Set(2, 2);
+			intcode.Run();
+
+			return intcode.Get(0);
+		}
+
+		public int Part2()
+		{
+			for (int noun = 0; noun < 100; noun++)
+			{
+				for (int verb = 0; verb < 100; verb++)
+				{
+					try
+					{
+						var intcode = new Computer(_input.ToArray());
+						intcode.Set(1, noun);
+						intcode.Set(2, verb);
+						intcode.Run();
+
+						if (intcode.Get(0) == 19690720)
+						{
+							return (noun * 100) + verb;
+						}
+					}
+					catch
+					{
+						// ignore
+					}
+				}
+			}
+
+			throw new Exception("not found");
+		}
+
+		public class Computer
+		{
+			private readonly int[] _memory;
+			private int _counter;
+
+			public Computer(int[] memory)
+			{
+				_memory = memory;
+			}
+
+			public int Get(int address)
+			{
+				return _memory[address];
+			}
+
+			public void Set(int address, int value)
+			{
+				_memory[address] = value;
+			}
+
+			public void Run()
+			{
+				while (true)
+				{
+					if (_memory[_counter] == 99)
+					{
+						return;
+					}
+
+					var pos1 = _memory[_counter + 1];
+					var pos2 = _memory[_counter + 2];
+					var pos3 = _memory[_counter + 3];
+
+					switch (_memory[_counter])
+					{
+						case 1:
+							_memory[pos3] = _memory[pos1] + _memory[pos2];
+							break;
+						case 2:
+							_memory[pos3] = _memory[pos1] * _memory[pos2];
+							break;
+						default:
+							throw new InvalidOperationException();
+					}
+
+					_counter += 4;
+				}
+			}
+		}
+	}
+}
