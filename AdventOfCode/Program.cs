7aa6a478a216c9ae8ba12832352189f5fa3300f2
Martin Wallgren <martin@orangeworks.se>
2019-12-04 12:13:57 +0100

    Embed input files

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
+using System.Reflection;
 
 namespace AdventOfCode
 {
@@ -8,21 +9,33 @@ namespace AdventOfCode
 	{
 		public static void Main(string[] args)
 		{
-			if (args.Length < 4)
+			if (args.Length < 3)
 			{
-				Console.WriteLine("AdventOfCode [Year] [Day] [Part] [Input]");
+				Console.WriteLine("AdventOfCode [Year] [Day] [Part] (Input)");
 				return;
 			}
 
 			var year = Int32.Parse(args[0], CultureInfo.InvariantCulture);
 			var day = Int32.Parse(args[1], CultureInfo.InvariantCulture);
 			var part = Int32.Parse(args[2], CultureInfo.InvariantCulture);
-			var init = new object[] { File.ReadAllText(args[3]) };
+			var init = args.Length == 4
+				? new object[] { File.ReadAllText(args[3]) }
+				: new object[] { GetEmbeddedInput(year, day) };
 
-			var instance = Activator.CreateInstance(Type.GetType($"AdventOfCode.Year{year}.Day{day}"), init);
-			var result = instance.GetType().GetMethod($"Part{part}").Invoke(instance, Type.EmptyTypes);
+			var type = Type.GetType($"AdventOfCode.Year{year}.Day{day}");
+			var instance = Activator.CreateInstance(type, init);
+			var result = type.GetMethod($"Part{part}").Invoke(instance, Type.EmptyTypes);
 
 			Console.WriteLine("Result: {0}", result);
+		}
+
+		private static string GetEmbeddedInput(int year, int day)
+		{
+			using var stream = Assembly.GetExecutingAssembly()
+				.GetManifestResourceStream($"AdventOfCode.Year{year}.Inputs.Day{day}.txt");
+			using var reader = new StreamReader(stream);
+
+			return reader.ReadToEnd();
 		}
 	}
 }
