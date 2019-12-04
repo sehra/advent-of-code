d0cf5ae5f8e8e0a17a37c888de89a5796c5326da
Martin Wallgren <martin@orangeworks.se>
2019-12-04 11:13:56 +0100

    Day 4

---
+++ AdventOfCode.Tests/Year2019/Day4Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2019
+{
+	[TestClass]
+	public class Day4Tests
+	{
+		[DataTestMethod]
+		[DataRow("111111", true)]
+		[DataRow("223450", false)]
+		[DataRow("123789", false)]
+		public void Part1(string value, bool expected)
+		{
+			Assert.AreEqual(expected, Day4.IsValid1(value));
+		}
+
+		[DataTestMethod]
+		[DataRow("112233", true)]
+		[DataRow("123444", false)]
+		[DataRow("111122", true)]
+		public void Part2(string value, bool expected)
+		{
+			Assert.AreEqual(expected, Day4.IsValid2(value));
+		}
+	}
+}
