cc6935a5fbc723b2db45cdae7e257e7d179bd43e
Martin Wallgren <martin@orangeworks.se>
2019-12-17 21:10:27 +0100

    Day 17

--- AdventOfCode/Extensions.cs
+++ AdventOfCode/Extensions.cs
@@ -6,6 +6,26 @@ namespace AdventOfCode
 {
 	public static class Extensions
 	{
+		public static IEnumerable<IEnumerable<T>> GroupWhile<T>(this IEnumerable<T> items, Func<T, T, bool> condition)
+		{
+			var prev = items.First();
+			var list = new List<T> { prev };
+
+			foreach (var item in items.Skip(1))
+			{
+				if (!condition(prev, item))
+				{
+					yield return list;
+					list = new List<T>();
+				}
+
+				list.Add(item);
+				prev = item;
+			}
+
+			yield return list;
+		}
+
 		public static IEnumerable<IEnumerable<T>> Permutations<T>(this IEnumerable<T> items)
 		{
 			if (items == null)
