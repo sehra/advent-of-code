fdbecd21665b9e2f6971e6ab6c4928374c06163e
Martin Wallgren <martin@orangeworks.se>
2019-12-27 23:25:53 +0100

    Day 25

---
+++ AdventOfCode/Year2019/Day25.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2019
+{
+	public class Day25
+	{
+		private readonly string _input;
+
+		public Day25(string input)
+		{
+			_input = input;
+		}
+
+		public async Task<int> Part1()
+		{
+			var signal = new SemaphoreSlim(0);
+			var input = Channel.CreateUnbounded<BigInteger>();
+			var output = new List<char>();
+			var intcode = new IntcodeComputer(_input)
+			{
+				Input = () => input.Reader.ReadAsync().AsTask(),
+				Output = value => { output.Add((char)value); return Task.CompletedTask; },
+			};
+
+			var commands = new[]
+			{
+				"east",
+				"take whirled peas",
+				"north",
+				"west",
+				"south",
+				"take antenna",
+				"north",
+				"east",
+				"south",
+				"east",
+				"north",
+				"take prime number",
+				"south",
+				"west",
+				"west",
+				"north",
+				"take fixed point",
+				"north",
+				"east",
+				"south",
+			};
+
+			foreach (var command in commands)
+			{
+				foreach (var c in Encoding.ASCII.GetBytes(command))
+				{
+					input.Writer.TryWrite(c);
+				}
+
+				input.Writer.TryWrite('\n');
+			}
+
+			await intcode.RunAsync();
+
+			return Int32.Parse(Regex.Match(new string(output.ToArray()), @"(\d{7})").Value);
+		}
+	}
+}
