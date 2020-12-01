f7caa977ac2bd7dc1728ea8ab7033d4d03fde616
Martin Wallgren <martin@orangeworks.se>
2020-12-01 09:37:34 +0100

    Make the script download the input

--- CreateDay.ps1
+++ CreateDay.ps1
@@ -1,17 +1,22 @@
 param (
-    $Year = (Get-Date).Year,
-    $Day = (Get-Date).Day
+    $year = (Get-Date).Year,
+    $day = (Get-Date).Day
 )
 
-$Code = @"
+$code = @"
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace AdventOfCode.Year${Year}
+namespace AdventOfCode.Year${year}
 {
-	public class Day${Day}
+	public class Day${day}
 	{
-		public Day${Day}(string input)
+		private readonly string _input;
+
+		public Day${day}(string input)
 		{
+			_input = input;
 		}
 
 		public int Part1()
@@ -27,31 +32,40 @@ namespace AdventOfCode.Year${Year}
 }
 "@;
 
-$Test = @"
+$test = @"
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AdventOfCode.Year${Year}
+namespace AdventOfCode.Year${year}
 {
 	[TestClass]
-	public class Day${Day}Tests
+	public class Day${day}Tests
 	{
 		[DataTestMethod]
-		[DataRow("", 0)]
-		public void Part1(string input, int expected)
+		[DataRow(0, "")]
+		public void Part1(int expected, string input)
 		{
-			Assert.AreEqual(expected, new Day${Day}(input).Part1());
+			Assert.AreEqual(expected, new Day${day}(input).Part1());
 		}
 
 		[DataTestMethod]
-		[DataRow("", 0)]
-		public void Part2(string input, int expected)
+		[DataRow(0, "")]
+		public void Part2(int expected, string input)
 		{
-			Assert.AreEqual(expected, new Day${Day}(input).Part2());
+			Assert.AreEqual(expected, new Day${day}(input).Part2());
 		}
 	}
 }
 "@
 
-Out-File "${PSScriptRoot}/AdventOfCode/Year${Year}/Day${Day}.cs" -InputObject $Code -Encoding UTF8
-Out-File "${PSScriptRoot}/AdventOfCode/Year${Year}/Inputs/Day${Day}.txt" -InputObject "" -Encoding UTF8
-Out-File "${PSScriptRoot}/AdventOfCode.Tests/Year${Year}/Day${Day}Tests.cs" -InputObject $Test -Encoding UTF8
+Out-File "${PSScriptRoot}/AdventOfCode/Year${year}/Day${day}.cs" -InputObject $code -Encoding UTF8
+Out-File "${PSScriptRoot}/AdventOfCode.Tests/Year${year}/Day${day}Tests.cs" -InputObject $test -Encoding UTF8
+
+$cookie = New-Object System.Net.Cookie
+$cookie.Domain = ".adventofcode.com"
+$cookie.Name = "session"
+$cookie.Value = $env:ADVENTOFCODE_SESSION
+
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$session.Cookies.Add($cookie)
+
+Invoke-WebRequest "https://adventofcode.com/${year}/day/${day}/input" -WebSession $session -OutFile "${PSScriptRoot}/AdventOfCode/Year${year}/Inputs/Day${day}.txt"
