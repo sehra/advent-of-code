2fe790b72fab69f018de7448151fc6075b9b8f8f
Martin Wallgren <martin@orangeworks.se>
2020-12-05 13:08:09 +0100

    Day 5

---
+++ AdventOfCode.Tests/Year2020/Day5Tests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2020
+{
+	[TestClass]
+	public class Day5Tests
+	{
+		[DataTestMethod]
+		[DataRow(357, "FBFBBFFRLR")]
+		[DataRow(567, "BFFFBBFRRR")]
+		[DataRow(119, "FFFBBBFRRR")]
+		[DataRow(820, "BBFFBBFRLL")]
+		public void Part1(int expected, string input)
+		{
+			Assert.AreEqual(expected, new Day5(input).Part1());
+		}
+	}
+}
---
+++ AdventOfCode/Year2020/Day5.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2020
+{
+	public class Day5
+	{
+		private readonly string[] _input;
+
+		public Day5(string input)
+		{
+			_input = input.ToLines();
+		}
+
+		public int Part1()
+		{
+			return _input.Select(Decode).Max();
+		}
+
+		public int Part2()
+		{
+			var seats = _input.Select(Decode).OrderBy(x => x).ToArray();
+
+			for (int i = 0; i < seats.Length - 1; i++)
+			{
+				if (seats[i] + 1 != seats[i + 1])
+				{
+					return seats[i] + 1;
+				}
+			}
+
+			throw new Exception("not found");
+		}
+
+		private int Decode(string value)
+		{
+			value = value
+				.Replace('F', '0')
+				.Replace('B', '1')
+				.Replace('L', '0')
+				.Replace('R', '1');
+
+			return Convert.ToInt32(value, 2);
+		}
+	}
+}
