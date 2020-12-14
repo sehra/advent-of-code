3af85d2bc51e2698b7c6bd0b7e78b140634e44a7
Martin Wallgren <martin@orangeworks.se>
2020-12-14 17:00:16 +0100

    Day 13

---
+++ AdventOfCode.Tests/Year2020/Day13Tests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2020
+{
+	[TestClass]
+	public class Day13Tests
+	{
+		[DataTestMethod]
+		[DataRow(295, "939\n7,13,x,x,59,x,31,19\n")]
+		public void Part1(int expected, string input)
+		{
+			Assert.AreEqual(expected, new Day13(input).Part1());
+		}
+
+		[DataTestMethod]
+		[DataRow(1068781, "7,13,x,x,59,x,31,19")]
+		[DataRow(3417, "17,x,13,19")]
+		[DataRow(754018, "67,7,59,61")]
+		[DataRow(779210, "67,x,7,59,61")]
+		[DataRow(1261476, "67,7,x,59,61")]
+		[DataRow(1202161486, "1789,37,47,1889")]
+		public void Part2(int expected, string input)
+		{
+			Assert.AreEqual(expected, new Day13($"0\n{input}\n").Part2());
+		}
+	}
+}
---
+++ AdventOfCode/Year2020/Day13.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2020
+{
+	public class Day13
+	{
+		private readonly string[] _input;
+
+		public Day13(string input)
+		{
+			_input = input.ToLines();
+		}
+
+		public int Part1()
+		{
+			var ts = _input[0].ToInt32();
+			var bus = _input[1].Split(',')
+				.Where(id => id != "x")
+				.Select(Int32.Parse)
+				.Select(id => (id, delay: id - (ts % id)))
+				.OrderBy(bus => bus.delay)
+				.First();
+
+			return bus.id * bus.delay;
+		}
+
+		public long Part2()
+		{
+			var busses = _input[1].Split(',')
+				.Select((id, i) => (id, i))
+				.Where(bus => bus.id != "x")
+				.Select(bus => (id: bus.id.ToInt32(), delta: bus.i))
+				.ToArray();
+
+			var time = 0L;
+			var jump = busses[0].id;
+
+			foreach (var (id, delta) in busses.Skip(1))
+			{
+				while ((time + delta) % id != 0)
+				{
+					time += jump;
+				}
+
+				jump *= id;
+			}
+
+			return time;
+		}
+	}
+}
