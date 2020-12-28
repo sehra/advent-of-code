01a248a140f91a9aa81daec1e738185713c4a608
Martin Wallgren <martin@orangeworks.se>
2020-12-28 11:53:35 +0100

    Day 23

---
+++ AdventOfCode.Tests/Year2020/Day23Tests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2020
+{
+	[TestClass]
+	public class Day23Tests
+	{
+		private const string Input = "389125467";
+
+		[DataTestMethod]
+		[DataRow("92658374", Input, 10)]
+		[DataRow("67384529", Input, 100)]
+		public void Part1(string expected, string input, int moves)
+		{
+			Assert.AreEqual(expected, new Day23(input).Part1(moves));
+		}
+
+		[DataTestMethod]
+		[DataRow(149245887792, Input)]
+		public void Part2(long expected, string input)
+		{
+			Assert.AreEqual(expected, new Day23(input).Part2());
+		}
+	}
+}
