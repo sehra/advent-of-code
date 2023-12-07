bddb25518db9d9715d82263252cc7594b0ffea05
Martin Wallgren <martin@orangeworks.se>
2023-12-07 15:19:43 +0100

    Optimize Day 7

--- AdventOfCode.Tests/Year2023/Day7Tests.cs
+++ AdventOfCode.Tests/Year2023/Day7Tests.cs
@@ -3,7 +3,7 @@ namespace AdventOfCode.Year2023;
 [TestClass]
 public class Day7Tests
 {
-	private const string Input =
+	private const string Input1 =
 		"""
 		32T3K 765
 		T55J5 684
@@ -11,16 +11,40 @@ public class Day7Tests
 		KTJJT 220
 		QQQJA 483
 		""";
+	private const string Input2 =
+		"""
+		2345A 1
+		Q2KJJ 13
+		Q2Q2Q 19
+		T3T3J 17
+		T3Q33 11
+		2345J 3
+		J345A 2
+		32T3K 5
+		T55J5 29
+		KK677 7
+		KTJJT 34
+		QQQJA 31
+		JJJJJ 37
+		JAAAA 43
+		AAAAJ 59
+		AAAAA 61
+		2AAAA 23
+		2JJJJ 53
+		JJJJ2 41
+		""";
 
 	[DataTestMethod]
-	[DataRow(6440, Input)]
+	[DataRow(6440, Input1)]
+	[DataRow(6592, Input2)]
 	public void Part1(int expected, string input)
 	{
 		Assert.AreEqual(expected, new Day7(input.ToLines()).Part1());
 	}
 
 	[DataTestMethod]
-	[DataRow(5905, Input)]
+	[DataRow(5905, Input1)]
+	[DataRow(6839, Input2)]
 	public void Part2(int expected, string input)
 	{
 		Assert.AreEqual(expected, new Day7(input.ToLines()).Part2());
--- AdventOfCode/Year2023/Day7.cs
+++ AdventOfCode/Year2023/Day7.cs
@@ -6,38 +6,53 @@ public class Day7(string[] input)
 
 	public int Part2() => Solve(true);
 
-	private int Solve(bool jokers) => Parse(jokers)
-		.Order()
-		.Index(1)
-		.Sum(kv => kv.Key * kv.Value.Bid);
-
-	private int Score(string hand, bool jokers)
+	private int Solve(bool jokers)
 	{
-		var counts = (jokers ? hand.Where(c => c is not 'J') : hand)
-			.GroupBy(c => c)
-			.Select(g => g.Count())
-			.OrderDescending()
-			.ToArray();
+		var hands = new List<long>(input.Length);
 
-		if (jokers)
+		foreach (var line in input)
 		{
-			var extra = hand.Count(c => c is 'J');
+			var score = Score(line.AsSpan(0, 5), jokers);
+			var order = Order(line.AsSpan(0, 5), jokers);
+			var bid = line.AsSpan(6).ToInt64();
+			hands.Add(score << 52 | order << 32 | bid);
+		}
 
-			if (counts.Length is 0)
+		hands.Sort();
+
+		return hands.Index(1).Sum(x => x.Key * (int)(x.Value & uint.MaxValue));
+	}
+
+	private long Score(ReadOnlySpan<char> hand, bool jokers)
+	{
+		Span<int> counts = stackalloc int[16];
+
+		for (int i = 0; i < hand.Length; i++)
+		{
+			var card = hand[i] switch
 			{
-				counts = [5];
-			}
-			else
+				'A' => 14,
+				'K' => 13,
+				'Q' => 12,
+				'J' => jokers ? 0 : 11,
+				'T' => 10,
+				var c => c - '0',
+			};
+
+			if (card > 0)
 			{
-				counts[0] += extra;
+				counts[card]++;
 			}
 		}
 
+		counts.Sort((a, b) => b - a);
+		counts[0] += jokers ? hand.Count('J') : 0;
+
 		return counts switch
 		{
-			[5] => 6,
+			[5, ..] => 6,
 			[4, ..] => 5,
-			[3, 2] => 4,
+			[3, 2, ..] => 4,
 			[3, ..] => 3,
 			[2, 2, ..] => 2,
 			[2, ..] => 1,
@@ -45,26 +60,17 @@ public class Day7(string[] input)
 		};
 	}
 
-	private readonly record struct Hand(string Cards, bool Jokers) : IComparable<Hand>
+	private static long Order(ReadOnlySpan<char> hand, bool jokers)
 	{
-		public int CompareTo(Hand other) => Order().CompareTo(other.Order());
+		var values = jokers ? "J23456789TQKA" : "23456789TJQKA";
+		var order = 0;
 
-		private (int, int, int, int, int) Order()
+		for (int i = 0; i < hand.Length; i++)
 		{
-			var values = Jokers ? "J23456789TQKA" : "23456789TJQKA";
-
-			return (
-				values.IndexOf(Cards[0]),
-				values.IndexOf(Cards[1]),
-				values.IndexOf(Cards[2]),
-				values.IndexOf(Cards[3]),
-				values.IndexOf(Cards[4])
-			);
+			order <<= 4;
+			order |= values.IndexOf(hand[i]);
 		}
-	}
 
-	private (int Score, Hand, int Bid)[] Parse(bool jokers) => input
-		.Select(line => line.Split(' '))
-		.Select(line => (Score(line[0], jokers), new Hand(line[0], jokers), line[1].ToInt32()))
-		.ToArray();
+		return order;
+	}
 }
