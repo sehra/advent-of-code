224b39565556412fb4cc031a59ee56beff8a8570
Martin Wallgren <martin@orangeworks.se>
2023-12-23 18:08:39 +0100

    Day 22 Part 2

--- AdventOfCode/Year2023/Day23.cs
+++ AdventOfCode/Year2023/Day23.cs
@@ -4,9 +4,9 @@ public class Day23(string[] input)
 {
 	public int Part1()
 	{
-		var (map, end) = Parse();
+		var (map, beg, end) = Parse();
 
-		return Count(new(0, 1), [new(0, 1)]);
+		return Count(beg, [beg]);
 
 		int Count(Point pos, HashSet<Point> path)
 		{
@@ -25,7 +25,7 @@ public class Day23(string[] input)
 				_ => throw new Exception("type?"),
 			};
 
-			var dist = -1;
+			var dist = 0;
 
 			foreach (var next in moves)
 			{
@@ -42,7 +42,78 @@ public class Day23(string[] input)
 
 	public int Part2()
 	{
-		throw new NotImplementedException();
+		var (map, beg, end) = Parse();
+		var graph = new Dictionary<Point, Dictionary<Point, int>>();
+
+		foreach (var key in map.Keys)
+		{
+			graph.Add(key, []);
+
+			foreach (var pos in "UDLR".Select(key.Step))
+			{
+				if (map.ContainsKey(pos))
+				{
+					graph[key].Add(pos, 1);
+				}
+			}
+		}
+
+		foreach (var key in map.Keys)
+		{
+			if (!graph.ContainsKey(key))
+			{
+				continue;
+			}
+
+			var nbors = graph[key];
+
+			if (nbors.Count is 0 || (nbors.Count is 1 && !(key == beg || key == end)))
+			{
+				graph.Remove(key);
+
+				foreach (var (nbor, _) in nbors)
+				{
+					graph[nbor].Remove(key);
+				}
+			}
+			else if (nbors.Count is 2)
+			{
+				var dist = nbors.Values.Sum();
+				var fst = nbors.Keys.ElementAt(0);
+				var snd = nbors.Keys.ElementAt(1);
+				graph[fst].Add(snd, dist);
+				graph[snd].Add(fst, dist);
+				graph.Remove(key);
+
+				foreach (var (nbor, _) in nbors)
+				{
+					graph[nbor].Remove(key);
+				}
+			}
+		}
+
+		return Count(beg, new() { [beg] = 0 });
+
+		int Count(Point pos, Dictionary<Point, int> path)
+		{
+			if (pos == end)
+			{
+				return path.Values.Sum();
+			}
+
+			var dist = 0;
+
+			foreach (var nbor in graph[pos])
+			{
+				if (path.TryAdd(nbor.Key, nbor.Value))
+				{
+					dist = Math.Max(dist, Count(nbor.Key, path));
+					path.Remove(nbor.Key);
+				}
+			}
+
+			return dist;
+		}
 	}
 
 	private readonly record struct Point(int R, int C)
@@ -57,7 +128,7 @@ public class Day23(string[] input)
 		};
 	}
 
-	private (Dictionary<Point, char> Map, Point End) Parse()
+	private (Dictionary<Point, char> Map, Point Beg, Point End) Parse()
 	{
 		var map = new Dictionary<Point, char>();
 
@@ -74,6 +145,6 @@ public class Day23(string[] input)
 			}
 		}
 
-		return (map, new(input.Length - 1, input[^1].Length - 2));
+		return (map, new(0, 1), new(input.Length - 1, input[^1].Length - 2));
 	}
 }
