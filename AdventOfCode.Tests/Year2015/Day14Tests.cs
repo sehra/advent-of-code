7ca2b511d2ae1ec9ed3eed9e798af412e5705303
Martin Wallgren <martin@orangeworks.se>
2023-12-27 09:54:20 +0100

    Day 14

---
+++ AdventOfCode.Tests/Year2015/Day14Tests.cs
@@ -0,0 +1,25 @@
+namespace AdventOfCode.Year2015;
+
+[TestClass]
+public class Day14Tests
+{
+	private const string Input =
+		"""
+		Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+		Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+		""";
+
+	[DataTestMethod]
+	[DataRow(1120, Input)]
+	public void Part1(int expected, string input)
+	{
+		Assert.AreEqual(expected, new Day14(input.ToLines()).Part1(1000));
+	}
+
+	[DataTestMethod]
+	[DataRow(689, Input)]
+	public void Part2(int expected, string input)
+	{
+		Assert.AreEqual(expected, new Day14(input.ToLines()).Part2(1000));
+	}
+}
