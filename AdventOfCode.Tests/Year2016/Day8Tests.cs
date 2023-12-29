6c3cdab75d5337fbc367dbbc00af85a027290882
Martin Wallgren <martin@orangeworks.se>
2023-12-30 00:30:15 +0100

    Day 8

---
+++ AdventOfCode.Tests/Year2016/Day8Tests.cs
@@ -0,0 +1,20 @@
+namespace AdventOfCode.Year2016;
+
+[TestClass]
+public class Day8Tests
+{
+	private const string Input =
+		"""
+		rect 3x2
+		rotate column x=1 by 1
+		rotate row y=0 by 4
+		rotate column x=1 by 1
+		""";
+
+	[DataTestMethod]
+	[DataRow(6, Input)]
+	public void Part1(int expected, string input)
+	{
+		Assert.AreEqual(expected, new Day8(input.ToLines()).Part1(7, 3));
+	}
+}
