2393069f8c27d119eb34d7cc322cfd3fa885dab0
Martin Wallgren <martin@orangeworks.se>
2023-12-31 00:44:08 +0100

    Day 20

---
+++ AdventOfCode.Tests/Year2016/Day20Tests.cs
@@ -0,0 +1,26 @@
+namespace AdventOfCode.Year2016;
+
+[TestClass]
+public class Day20Tests
+{
+	private const string Input =
+		"""
+		5-8
+		0-2
+		4-7
+		""";
+
+	[DataTestMethod]
+	[DataRow(3u, Input)]
+	public void Part1(uint expected, string input)
+	{
+		Assert.AreEqual(expected, new Day20(input.ToLines()).Part1(0, 9));
+	}
+
+	[DataTestMethod]
+	[DataRow(2, Input)]
+	public void Part2(long expected, string input)
+	{
+		Assert.AreEqual(expected, new Day20(input.ToLines()).Part2(0, 9));
+	}
+}
