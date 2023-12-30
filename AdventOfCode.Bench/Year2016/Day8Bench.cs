268cb63590ce13cb45c6bfde7e06b94f9f27cce0
Martin Wallgren <martin@orangeworks.se>
2023-12-30 01:28:24 +0100

    Fix build

--- AdventOfCode.Bench/Year2016/Day8Bench.cs
+++ AdventOfCode.Bench/Year2016/Day8Bench.cs
@@ -3,17 +3,17 @@ namespace AdventOfCode.Year2016;
 [MemoryDiagnoser]
 public class Day8Bench
 {
-	private string _input;
+	private string[] _input;
 
 	[GlobalSetup]
 	public void Setup()
 	{
-		_input = Program.GetEmbeddedInput(2016, 8);
+		_input = Program.GetEmbeddedInput(2016, 8).ToLines();
 	}
 
 	[Benchmark]
 	public int Part1() => new Day8(_input).Part1();
 
 	[Benchmark]
-	public int Part2() => new Day8(_input).Part2();
+	public string Part2() => new Day8(_input).Part2();
 }
