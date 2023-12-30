e1938c93c8a0258a6ec1c4208b80ab2c306116cb
Martin Wallgren <martin@orangeworks.se>
2023-12-30 14:12:46 +0100

    Day 12

---
+++ AdventOfCode.Tests/Year2016/Day12Tests.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode.Year2016;
+
+[TestClass]
+public class Day12Tests
+{
+	private const string Input =
+		"""
+		cpy 41 a
+		inc a
+		inc a
+		dec a
+		jnz a 2
+		dec a
+		""";
+
+	[DataTestMethod]
+	[DataRow(42, Input)]
+	public void Part1(int expected, string input)
+	{
+		Assert.AreEqual(expected, new Day12(input.ToLines()).Part1());
+	}
+}
