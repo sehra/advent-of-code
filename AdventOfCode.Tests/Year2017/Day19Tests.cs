55254a2d8e96f8a6dfdb03ec8d1dd147e6b4807c
Martin Wallgren <martin@orangeworks.se>
2024-01-04 23:09:32 +0100

    Day 19

---
+++ AdventOfCode.Tests/Year2017/Day19Tests.cs
@@ -0,0 +1,29 @@
+namespace AdventOfCode.Year2017;
+
+[TestClass]
+public class Day19Tests
+{
+	private const string Input =
+		"""
+		     |          
+		     |  +--+    
+		     A  |  C    
+		 F---|----E|--+ 
+		     |  |  |  D 
+		     +B-+  +--+ 
+		""";
+
+	[DataTestMethod]
+	[DataRow("ABCDEF", Input)]
+	public void Part1(string expected, string input)
+	{
+		Assert.AreEqual(expected, new Day19(input).Part1());
+	}
+
+	[DataTestMethod]
+	[DataRow(38, Input)]
+	public void Part2(int expected, string input)
+	{
+		Assert.AreEqual(expected, new Day19(input).Part2());
+	}
+}
