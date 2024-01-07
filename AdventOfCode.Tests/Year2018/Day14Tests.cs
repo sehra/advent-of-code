da09d9f39b25286fa11d9182c8012a75b1dd0d19
Martin Wallgren <martin@orangeworks.se>
2024-01-07 12:29:28 +0100

    Day 14

---
+++ AdventOfCode.Tests/Year2018/Day14Tests.cs
@@ -0,0 +1,25 @@
+namespace AdventOfCode.Year2018;
+
+[TestClass]
+public class Day14Tests
+{
+	[DataTestMethod]
+	[DataRow("5158916779", "9")]
+	[DataRow("0124515891", "5")]
+	[DataRow("9251071085", "18")]
+	[DataRow("5941429882", "2018")]
+	public void Part1(string expected, string input)
+	{
+		Assert.AreEqual(expected, new Day14(input).Part1());
+	}
+
+	[DataTestMethod]
+	[DataRow(9, "51589")]
+	[DataRow(5, "01245")]
+	[DataRow(18, "92510")]
+	[DataRow(2018, "59414")]
+	public void Part2(int expected, string input)
+	{
+		Assert.AreEqual(expected, new Day14(input).Part2());
+	}
+}
