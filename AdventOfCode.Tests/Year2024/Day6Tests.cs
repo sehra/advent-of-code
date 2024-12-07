b5b2773bd7fa8555a9664e3bb0507e8333355435
Martin Wallgren <martin@orangeworks.se>
2024-12-07 09:48:39 +0100

    Code clean-up

--- AdventOfCode.Tests/Year2024/Day6Tests.cs
+++ AdventOfCode.Tests/Year2024/Day6Tests.cs
@@ -3,8 +3,7 @@ namespace AdventOfCode.Year2024;
 [TestClass]
 public class Day6Tests
 {
-	[DataTestMethod]
-	[DataRow(41,
+	private const string Input =
 		"""
 		....#.....
 		.........#
@@ -16,26 +15,17 @@ public class Day6Tests
 		........#.
 		#.........
 		......#...
-		""")]
+		""";
+
+	[DataTestMethod]
+	[DataRow(41, Input)]
 	public void Part1(int expected, string input)
 	{
 		Assert.AreEqual(expected, new Day6(input.ToLines()).Part1());
 	}
 
 	[DataTestMethod]
-	[DataRow(6,
-		"""
-		....#.....
-		.........#
-		..........
-		..#.......
-		.......#..
-		..........
-		.#..^.....
-		........#.
-		#.........
-		......#...
-		""")]
+	[DataRow(6, Input)]
 	public void Part2(int expected, string input)
 	{
 		Assert.AreEqual(expected, new Day6(input.ToLines()).Part2());
