4b2ddedcfba6270e0a24681cfa9ea6a10884c65f
Martin Wallgren <martin@orangeworks.se>
2025-12-15 00:07:16 +0100

    Optimize Day 7

--- AdventOfCode/Year2025/Day7.cs
+++ AdventOfCode/Year2025/Day7.cs
@@ -2,43 +2,30 @@ namespace AdventOfCode.Year2025;
 
 public class Day7(string[] input)
 {
-	public int Part1()
+	public int Part1() => Solve().Item1;
+
+	public long Part2() => Solve().Item2;
+
+	private (int, long) Solve()
 	{
-		var beams = new HashSet<int>() { input[0].IndexOf('S') };
+		var beams = new long[input[0].Length];
+		beams[input[0].IndexOf('S')] = 1;
 		var count = 0;
 
 		foreach (var line in input)
 		{
 			for (int i = 0; i < line.Length; i++)
 			{
-				if (line[i] is '^' && beams.Remove(i))
+				if (line[i] is '^' && beams[i] > 0)
 				{
-					beams.Add(i - 1);
-					beams.Add(i + 1);
+					beams[i - 1] += beams[i];
+					beams[i + 1] += beams[i];
+					beams[i] = 0;
 					count++;
 				}
 			}
 		}
 
-		return count;
-	}
-
-	public long Part2()
-	{
-		var beams = new Dictionary<int, long>() { [input[0].IndexOf('S')] = 1 };
-
-		foreach (var line in input)
-		{
-			for (int i = 0; i < line.Length; i++)
-			{
-				if (line[i] is '^' && beams.Remove(i, out var count))
-				{
-					beams[i - 1] = beams.GetValueOrDefault(i - 1) + count;
-					beams[i + 1] = beams.GetValueOrDefault(i + 1) + count;
-				}
-			}
-		}
-
-		return beams.Values.Sum();
+		return (count, beams.Sum());
 	}
 }
